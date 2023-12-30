b6c7134ab91678a1a17a15c355f8cc3e310d1ab7
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-12-30 11:21:17 +0200

    Add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import GlobalStyles from './styles/GlobalStyles';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Heading as='h1'>Hello World</Heading>
+      <Row>
+        <h1>d</h1>
+        <h1>d</h1>
+      </Row>
+    </>
+  );
 }
 
 export default App;
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
 const sizes = {
   small: css`
@@ -47,3 +47,22 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button<{
+  variation?: 'primary' | 'secondary' | 'danger';
+  size?: 'small' | 'medium' | 'large';
+}>`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+
+  ${(props) => props.variation && variations[props.variation]}
+  ${(props) => props.size && sizes[props.size]}
+`;
+
+Button.defaultProps = {
+  variation: 'primary',
+  size: 'medium',
+};
+
+export default Button;
