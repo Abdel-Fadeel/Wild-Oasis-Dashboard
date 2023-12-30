2fccd8faee5335aa2ec2b8378e1598e62ea6ae7d
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-12-30 11:41:06 +0200

    Setup pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+// Pages
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+// Global Styles
 import GlobalStyles from './styles/GlobalStyles';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading as='h1'>Hello World</Heading>
-      <Row>
-        <h1>d</h1>
-        <h1>d</h1>
-      </Row>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to='dashboard' replace />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
