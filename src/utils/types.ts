4be00ba90e42015e37b9224d4d91badf3946603e
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2024-01-24 20:19:01 +0200

    Add bookings table

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -19,3 +19,22 @@ export type Option = {
   label: string;
   value: string;
 };
+export type BookingStatus = 'unconfirmed' | 'checked-in' | 'checked-out';
+
+export type Booking = {
+  id: number;
+  created_at: string;
+  startDate: string;
+  endDate: string;
+  numNights: number;
+  numGuests: number;
+  totalPrice: number;
+  status: BookingStatus;
+  cabins: {
+    name: string;
+  };
+  guests: {
+    fullName: string;
+    email: string;
+  };
+};
